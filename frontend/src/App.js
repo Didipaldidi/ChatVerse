3dbe926ff9f4cd6f21ce5c0818b537ad390924a8
Valdi <valdilaurentius@gmail.com>
2023-07-20 20:09:38 -0700

    connect the router for homepage and chatpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import { Button } from '@chakra-ui/react';
 import './App.css';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import HomePage from './pages/Homepage';
+import ChatPage from './pages/Chatpage';
 
 function App() {
   return (<div className='App'>
-    <Button colorScheme='blue'>Button</Button>
+    <Route path='/' component={HomePage} exact />
+    <Route path='/chats' component={ChatPage}/>
   </div>);
 }
 
