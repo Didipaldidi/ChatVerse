706dfe4a8cbdd0377bedad455899ba6244fa05ec
Valdi <valdilaurentius@gmail.com>
2023-07-20 19:14:47 -0700

    created react app frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 
 const app = express();
 
-app.listen(5000, console.log("Server Started on PORT 5000"));
+dotenv.config();
+
+app.get("/", (req,res) => {
+    res.send("API is Successfully")
+})
+
+app.get("/api/chat", (req,res) => {
+    res.send(chats)
+})
+
+const PORT = process.env.PORT;
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
