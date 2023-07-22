cfb6e6003aa4ca6abbdbb5f7eb875c3ffed30440
Valdi <valdilaurentius@gmail.com>
2023-07-22 14:08:22 -0700

    connected SideDrawer, ChatBox and MyChats to ChatPage

--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { ChatState } from '../Context/ChatProvider';
+import { Box } from '@chakra-ui/react';
+import SideDrawer from '../components/miscellaneous/SideDrawer';
+import MyChats from '../components/MyChats';
+import ChatBox from '../components/ChatBox';
 
 function ChatPage() {
-  return (
-    <div>
-      ChatPage
-    </div>
-  );
+  const { user } = ChatState();
+
+  return <div style={{ width: "100%"}}>
+    {user && <SideDrawer/>}
+    <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+      { user && <MyChats/>}
+      { user && <ChatBox/>}
+    </Box>
+  </div>;
 }
 
 export default ChatPage;
