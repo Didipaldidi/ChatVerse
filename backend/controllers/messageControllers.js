c0cc2300330457f6b24a70c7150e73e1dd7d7cab
Valdi <valdilaurentius@gmail.com>
2023-08-01 21:47:05 -0700

    modify messageRoutes and messageControllers to handel sending images

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Message = require("../models/messageModel");
 const User = require("../models/userModel");
 const Chat = require("../models/chatModel");
+const fs = require("fs");
 
 //@description     Get all Messages
 //@route           GET /api/Message/:chatId
@@ -22,22 +23,29 @@ const allMessages = asyncHandler(async (req, res) => {
 //@route           POST /api/Message/
 //@access          Protected
 const sendMessage = asyncHandler(async (req, res) => {
-  const { content, chatId } = req.body;
+  const { content, chatId, imageData, imageType } = req.body; // Receive imageData and imageType from the frontend
 
   if (!content || !chatId) {
     console.log("Invalid data passed into request");
     return res.sendStatus(400);
   }
 
-  var newMessage = {
+  let newMessage = {
     sender: req.user._id,
     content: content,
     chat: chatId,
   };
 
+  if (imageData && imageType) {
+    // Handle image upload and storage
+    const savedImage = saveImageToFS(imageData, imageType);
+    newMessage.image = savedImage;
+  }
+
   try {
     var message = await Message.create(newMessage);
 
+    // Populate the "sender" and "chat" fields with additional information
     message = await message.populate("sender", "name pic");
     message = await message.populate("chat");
     message = await User.populate(message, {
@@ -45,7 +53,7 @@ const sendMessage = asyncHandler(async (req, res) => {
       select: "name pic email",
     });
 
-    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+    await Chat.findByIdAndUpdate(chatId, { latestMessage: message });
 
     res.json(message);
   } catch (error) {
@@ -53,5 +61,33 @@ const sendMessage = asyncHandler(async (req, res) => {
     throw new Error(error.message);
   }
 });
+
+const saveImageToFS = (imageData, imageType) => {
+  try {
+    // Generate a unique filename for the image
+    const uniqueFilename = `${Date.now()}-${Math.round(Math.random() * 1e9)}.${imageType.split("/")[1]}`;
+
+    // Decode the Base64 image data into a Buffer
+    const imageBuffer = Buffer.from(imageData, "base64");
+
+    // Define the path to the directory where you want to save the images
+    const imageDir = "../images";
+
+    // Create the directory if it doesn't exist
+    if (!fs.existsSync(imageDir)) {
+      fs.mkdirSync(imageDir, { recursive: true });
+    }
+
+    // Save the image to the filesystem
+    fs.writeFileSync(`${imageDir}/${uniqueFilename}`, imageBuffer);
+
+    // Return the filename for saving in the database
+    return uniqueFilename;
+  } catch (error) {
+    // Handle errors appropriately
+    console.error("Error saving image:", error);
+    throw error;
+  }
+};
 
 module.exports = { allMessages, sendMessage };
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer"); // Import multer for handling image uploads
 const {
   allMessages,
   sendMessage,
@@ -7,10 +8,14 @@ const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+// Multer storage configuration for image uploads
+const storage = multer.memoryStorage(); // Use memory storage to handle image uploads as buffers
+const upload = multer({ storage: storage });
+
 // Route to get all messages of a specific chat
 router.route("/:chatId").get(protect, allMessages);
 
 // Route to send a new message to a chat
-router.route("/").post(protect, sendMessage);
+router.route("/").post(protect, upload.single("imageData"), sendMessage);
 
 module.exports = router;
